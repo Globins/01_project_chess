e5e1d165b507e2f38f0ea11036d62e99c9d0c974
Gordon Lobins <gordon.lobins@gmail.com>
2019-07-07 12:42:29 -0700

    Added black pawn prototype. Added grabbing mechanics.

--- 01_project_chess_ai/Assets/Script/pawn_mech.cs
+++ 01_project_chess_ai/Assets/Script/pawn_mech.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class pawn_mech : MonoBehaviour
 {
+	private bool selected = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,26 @@ public class pawn_mech : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    	if(Input.GetMouseButtonDown(0))
+        	onClick();
+        if(selected)
+        	move();
+    }
+    private void onClick()
+    {
+		Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		RaycastHit2D select = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePos), Vector2.zero);
+		if(select && select.transform.gameObject.tag == "Piece")
+		{
+			Debug.Log("Grabbed");
+
+			selected = true;
+		}
+    }
+    private void move()
+    {
+    	Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+    	mousePos.z += 10;
+    	transform.position = Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
